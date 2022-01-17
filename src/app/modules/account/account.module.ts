04b6a83a1ab2fc77a0853fa01f30bce525b89473
AlineRinquin <90188226+AlineRinquin@users.noreply.github.com>
2022-01-17 15:32:28 +0100

    ajout formulaire et service auth

--- src/app/modules/account/account.module.ts
+++ src/app/modules/account/account.module.ts
@@ -6,18 +6,15 @@ import { PageSigninComponent } from './pages/page-signin/page-signin.component';
 import { PageSignupComponent } from './pages/page-signup/page-signup.component';
 import { PageForgotPasswordComponent } from './pages/page-forgot-password/page-forgot-password.component';
 import { PageResetPasswordComponent } from './pages/page-reset-password/page-reset-password.component';
-
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     PageSigninComponent,
     PageSignupComponent,
     PageForgotPasswordComponent,
-    PageResetPasswordComponent
+    PageResetPasswordComponent,
   ],
-  imports: [
-    CommonModule,
-    AccountRoutingModule
-  ]
+  imports: [CommonModule, AccountRoutingModule, FormsModule],
 })
-export class AccountModule { }
+export class AccountModule {}
--- src/app/modules/account/pages/page-signup/page-signup.component.ts
+++ src/app/modules/account/pages/page-signup/page-signup.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-page-signup',
   templateUrl: './page-signup.component.html',
-  styleUrls: ['./page-signup.component.scss']
+  styleUrls: ['./page-signup.component.scss'],
 })
 export class PageSignupComponent implements OnInit {
+  constructor(private authService: AuthService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  public onSubmit(objectForm: any): void {
+    console.log('value : ', objectForm.form.value);
+    console.log('form : ', objectForm);
+    const email = objectForm.form.value.email;
+    const password = objectForm.form.value.password;
+
+    this.authService
+      .signup(email, password)
+      .subscribe((resp) => console.log(resp));
   }
-
 }
